1c11b641ad37a07989d1a0291e67fb75d03f560f
LarissaDeOliveiraMorais <33908280+LarissaDeOliveiraMorais@users.noreply.github.com>
2019-04-04 21:11:47 -0300

    Add files via upload

---
+++ src/config/database.js
@@ -0,0 +1,52 @@
+const sqlite3 = require('sqlite3').verbose();
+const bd = new sqlite3.Database('data.db');
+
+const USUARIOS_SCHEMA = `
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    nome_completo VARCHAR(40) NOT NULL UNIQUE, 
+    email VARCHAR(255) NOT NULL, 
+    senha VARCHAR(255) NOT NULL
+)
+`;
+
+const INSERIR_USUARIO_1 = 
+`
+INSERT INTO usuarios (
+    nome_completo, 
+    email,
+    senha
+) SELECT 'Celso', 'celso@gmail', '123' WHERE NOT EXISTS (SELECT * FROM usuarios WHERE email = 'celso@gmail')
+`;
+
+const LIVROS_SCHEMA = 
+`
+CREATE TABLE IF NOT EXISTS livros (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titulo TEXT NOT NULL, 
+    preco REAL NOT NULL,
+    descricao TEXT DEFAULT ('') NOT NULL
+)
+`;
+
+bd.serialize(() => {
+    bd.run("PRAGMA foreign_keys=ON");
+    bd.run(USUARIOS_SCHEMA);
+    bd.run(INSERIR_USUARIO_1);
+    bd.run(LIVROS_SCHEMA);
+
+
+    bd.each("SELECT * FROM usuarios", (err, usuario) => {
+        console.log('Usuario: ');
+        console.log(usuario);
+    });
+});
+
+process.on('SIGINT', () =>
+    bd.close(() => {
+        console.log('BD encerrado!');
+        process.exit(0);
+    })
+);
+
+module.exports = bd;
